d51bef7af8d9c34f4ddc76695ee0e5e8a67e0e82
nthain <nthain@google.com>
2018-08-09 17:37:56 -0230

    Responding to reviewer comments

--- perspective_reddit_bot/moderate_subreddit.py
+++ perspective_reddit_bot/moderate_subreddit.py
@@ -1,28 +1,42 @@
 """A reddit bot which uses the Perpsective API to help moderate subreddits.
 
 Args:
-      subreddit: (required) A positional argument for the subreddit to moderate.
+      subreddit: (required) A positional argument for the name of the subreddit to moderate.
       -rule_config_file: (optional) The file which contains moderation rules. Defaults to 'rules.yaml'
       -output_dir: (optional) If set, scores for all streamed commments are written to a file in this directory.
+
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 import argparse
 import os
 import time
 
-import pandas as pd
-import praw
-import yaml
-
-import perspective_client
-import score_dataset
 from creds import creds
+import pandas as pd
+import perspective_client
 from perspective_rule import Rule
+import praw
+import score_dataset
+import yaml
 
 TEXT_COLUMN = 'comment_text'
 
 LANGUAGE = 'en'
-#TODO(nthain): support automated language detection.
+# TODO(nthain): support automated language detection.
+
 
 def parse_rules(filepath):
   rules = []
@@ -30,27 +44,39 @@ def parse_rules(filepath):
   with open(filepath) as f:
     raw_rules = yaml.load_all(f)
     for r in raw_rules:
-      rules.append(Rule(r['perspective_score'], r['action'], r.get('report_reason')))
+      rules.append(Rule(r['perspective_score'],
+                        r['action'],
+                        r.get('report_reason')))
       models.extend(r['perspective_score'].keys())
   assert len(rules) > 0, 'Rules file empty!'
   return rules, models
+
 
 def bot_is_mod(reddit, subreddit):
   mods = subreddit.moderator()
   return reddit.user.me() in mods
 
 
-def score_subreddit(creds,
+def score_subreddit(creds_dict,
                     subreddit_name,
                     rules,
                     models,
-                    output_path = None):
+                    output_path=None):
+  """Scores a stream of commments via the perspective API and applies pre-defined moderation rules.
 
-  reddit = praw.Reddit(client_id = creds['client_id'],
-  					 client_secret = creds['client_secret'],
-  					 user_agent = creds['user_agent'],
-             username = creds['username'],
-             password = creds['password'])
+  Args:
+    creds_dict: (dict) A dictionary of API credentials for Perspective and Reddit.
+    subreddit_name: (str) The name of the subreddit to stream.
+    rules: (list) A list of rules to apply to each comment.
+    models: (list) A list of models that the API must call to apply rules.
+    output_path: (str, optional) If supplied, all comments and scores will be written to this path.
+  """
+
+  reddit = praw.Reddit(client_id=creds_dict['reddit_client_id'],
+                       client_secret=creds_dict['reddit_client_secret'],
+                       user_agent=creds_dict['reddit_user_agent'],
+                       username=creds_dict['reddit_username'],
+                       password=creds_dict['reddit_password'])
   subreddit = reddit.subreddit(subreddit_name)
 
   try:
@@ -64,7 +90,7 @@ def score_subreddit(creds,
     print('Bot is not moderator of subreddit.')
     print('Moderation actions will not be applied.')
 
-  perspective = perspective_client.PerspectiveClient(creds['perspective_api_key'])
+  perspective = perspective_client.PerspectiveClient(creds_dict['perspective_api_key'])
 
   for i, comment in enumerate(subreddit.stream.comments()):
     try:
@@ -79,7 +105,7 @@ def score_subreddit(creds,
                                                 TEXT_COLUMN,
                                                 models,
                                                 perspective,
-                                                language = LANGUAGE)
+                                                language=LANGUAGE)
       if output_path:
         with open(output_path, 'a') as f:
           scored_df.to_json(f, orient='records', lines=True)
@@ -93,7 +119,6 @@ def score_subreddit(creds,
           print('Action: %s' % rule.action_name)
           if subreddit_name in ['all', 'popular']:
             print('Subreddit: %s' % comment.subreddit)
-
 
           if mod_permissions:
             rule.apply_action(comment)
--- perspective_reddit_bot/perspective_rule.py
+++ perspective_reddit_bot/perspective_rule.py
@@ -1,40 +1,58 @@
 """A class for checking and applying moderation rules.
+
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
-import praw
 
-class Rule:
+class Rule(object):
   """A class for checking and applying moderation rules.
   Args:
     model_rules: (list) A list of model rule dictionaries as read from the rules yaml file.
     action_name: (str) The name of an action to take. Must be one of ['report', 'remove', 'spam']
     report_reason: (str) (optional) The reason for the report to provide to moderators.
-    
   """
+
   def __init__(self,
                model_rules,
                action_name,
-               report_reason = None):
+               report_reason=None):
     self.model_rules = model_rules
     self.action_name = action_name
     self.report_reason = report_reason
 
   def __str__(self):
-    rule_strings = ['%s %s' % (k,v) for k,v in self.model_rules.items()]
+    rule_strings = ['%s %s' % (k, v) for k, v in self.model_rules.items()]
     return '\n'.join(rule_strings)
 
   def check_model_rules(self, scored_df):
-    # Check there is at least one model rule
+    """Checks to see if a scored comment fulfills all of the conditions for this rule."""
+
+    # Currently only checks one comment at a time.
+    assert scored_df.shape[0] == 1
+
+    # Check there is at least one model rule.
     assert len(self.model_rules) > 0, 'No model thresholds provided!'
 
-    # Checks that all model conditions hold
+    # Checks that all model conditions hold.
     state = True
     for model, comparison in self.model_rules.items():
       assert len(comparison.split()) == 2
       comparator, threshold = comparison.split()
       state = state and self._compare(scored_df['score:%s'%model][0],
-                               comparator,
-                               float(threshold))
+                                      comparator,
+                                      float(threshold))
     return state
 
   def _compare(self, score, comparator, threshold):
@@ -45,13 +63,12 @@ class Rule:
     else:
       raise ValueError('Rule must have a ">" or "<".')
 
-
   def apply_action(self, comment):
     if self.action_name == 'report':
       comment.report(self.report_reason)
     elif self.action_name == 'remove':
       comment.mod.remove()
     elif self.action_name == 'spam':
-      comment.mod.remove(spame = True)
+      comment.mod.remove(spam=True)
     else:
       raise ValueError('Action "%s" not yet implemented.' % self.action_name)
