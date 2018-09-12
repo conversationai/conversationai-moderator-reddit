dccf0a6210b9fea17d05b75c878c220cb8c3dece
nthain <nthain@google.com>
2018-09-11 20:17:29 -0400

    updating check_mod_actions to handle missing comments

--- perspective_reddit_bot/check_mod_actions.py
+++ perspective_reddit_bot/check_mod_actions.py
@@ -34,15 +34,25 @@ def write_moderator_actions(reddit,
                             output_path,
                             hours_to_wait):
   record = json.loads(line)
-  comment = reddit.comment(record[id_key])
   bot_scored_time = datetime.strptime(record[timestamp_key], '%Y%m%d_%H%M%S')
   time_to_check = bot_scored_time + timedelta(hours=hours_to_wait)
   wait_until(time_to_check)
-  record['approved'] = comment.approved
-  record['removed'] = comment.removed
+  comment_id = record[id_key]
+  print(comment_id)
+  record['approved'], record['removed'] = check_approved_removed(reddit,
+                                                                 comment_id)
   with open(output_path, 'a') as o:
     json.dump(record, o)
     o.write('\n')
+
+
+def check_approved_removed(reddit, comment_id):
+  try:
+    comment = reddit.comment(comment_id)
+    return comment.approved, comment.removed
+  except Exception as e:
+    print('Skipping comment due to exception: %s' % e)
+    return None, None
 
 
 def wait_until(time_to_proceed):
