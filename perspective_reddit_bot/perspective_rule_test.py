886e6187ceecd66378699294296092ffe5f29bc3
john li <jetpack@google.com>
2018-10-05 13:51:44 -0400

    Fix module docstring for tests.

--- perspective_reddit_bot/perspective_rule_test.py
+++ perspective_reddit_bot/perspective_rule_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A class for checking and applying moderation rules."""
+"""perspective_rule tests"""
 
 from __future__ import absolute_import
 from __future__ import division
